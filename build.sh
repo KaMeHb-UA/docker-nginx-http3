d67d9b2f1913903c5945da2a1f6640aecd7b4663
KaMeHb-UA <marlock@etlgr.com>
2019-10-23 13:20:19 +0300

    upd

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ cd $(uname -m)
 HASH=$(docker build . | tail -n1 | grep -Po '[a-f0-9]{9,}')
 docker tag $HASH kamehb/nginx-http3:$(cat ../.version)
 docker push kamehb/nginx-http3
+docker tag $HASH kamehb/nginx-http3:latest
+docker push kamehb/nginx-http3:latest
